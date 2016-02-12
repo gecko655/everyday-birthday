0c3364a4f02b646635cb5b9968f3aec37b1f9949
gecko655 <aqwsedrft1234@yahoo.co.jp>
2016-02-12 17:24:19 +0900

    Use begin-ensure-end for closing selenium-driver

--- everyday-birthday.rb
+++ everyday-birthday.rb
@@ -10,34 +10,35 @@ year = ENV["Year"]
 
 logger.info("Initializing")
 driver = Selenium::WebDriver.for :remote, :url => "http://"+hostname+":4444/wd/hub", :desired_capabilities => :chrome
-driver.manage.timeouts.implicit_wait = 10
+begin
+  driver.manage.timeouts.implicit_wait = 10
 
+  logger.info("Accessing to the user page: "+twitterID)
+  driver.navigate.to "https://twitter.com/"+twitterID
 
-logger.info("Accessing to the user page: "+twitterID)
-driver.navigate.to "https://twitter.com/"+twitterID
+  logger.info("Authentication: "+twitterID)
+  signin_email = driver.find_element(:name => "session[username_or_email]")
+  signin_email.send_keys twitterID
+  driver.find_element(:name => "session[password]").send_keys(password)
+  signin_email.submit
 
-logger.info("Authentication: "+twitterID)
-signin_email = driver.find_element(:name => "session[username_or_email]")
-signin_email.send_keys twitterID
-driver.find_element(:name => "session[password]").send_keys(password)
-signin_email.submit
+  editButton = driver.find_element(:class => "UserActions-editButton")
+  editButton.click
 
-editButton = driver.find_element(:class => "UserActions-editButton")
-editButton.click
+  sleep 1
+  localtime = Time.now.getlocal("+09:00")
+  logger.info("Set "+twitterID+"'s birthday to "+year+"/"+localtime.month.to_s+"/"+localtime.day.to_s)
+  driver.find_element(:class => "BirthdateSelect-button").click
+  birthday = Selenium::WebDriver::Support::Select.new driver.find_element(:class, "BirthdateSelect-month")
+  birthday.select_by(:value, localtime.month.to_s)
+  birthday = Selenium::WebDriver::Support::Select.new driver.find_element(:class, "BirthdateSelect-day")
+  birthday.select_by(:value, localtime.day.to_s)
+  birthday = Selenium::WebDriver::Support::Select.new driver.find_element(:class, "BirthdateSelect-year")
+  birthday.select_by(:value, year.to_s)
 
-sleep 1
-localtime = Time.now.getlocal("+09:00")
-logger.info("Set "+twitterID+"'s birthday to "+year+"/"+localtime.month.to_s+"/"+localtime.day.to_s)
-driver.find_element(:class => "BirthdateSelect-button").click
-birthday = Selenium::WebDriver::Support::Select.new driver.find_element(:class, "BirthdateSelect-month")
-birthday.select_by(:value, localtime.month.to_s)
-birthday = Selenium::WebDriver::Support::Select.new driver.find_element(:class, "BirthdateSelect-day")
-birthday.select_by(:value, localtime.day.to_s)
-birthday = Selenium::WebDriver::Support::Select.new driver.find_element(:class, "BirthdateSelect-year")
-birthday.select_by(:value, year.to_s)
-
-driver.find_element(:class, "ProfilePage-saveButton").click
-logger.info("Saved")
-
-sleep 3 
-driver.quit
+  driver.find_element(:class, "ProfilePage-saveButton").click
+  logger.info("Saved")
+ensure 
+  sleep 3 
+  driver.quit
+end
