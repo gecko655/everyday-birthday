fdae3f21b913b3d63cc701d2e8bf2852920e8fbb
gecko655 <aqwsedrft1234@yahoo.co.jp>
2020-01-18 17:28:58 +0900

    finally close browser

--- index.ts
+++ index.ts
@@ -36,44 +36,46 @@ if(!moment(getISOFormat(year, month, day)).isValid()) {
   //めんどいからroot cronでpuppeteerを起動するようにしたら謎のoptionが必要になった
   //https://qiita.com/HeRo/items/9be64b559692e12cc109
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-  const page = await browser.newPage();
-  page.setDefaultNavigationTimeout(60 * 1000);
-  await page.goto(`https://twitter.com/${twitterID}`,
-      {waitUntil: ['load', 'networkidle0']});
-  console.log(`Logging in to ${twitterID}`);
-  await page.type('form.LoginForm input[name=session\\[username_or_email\\]]', twitterID);
-  await page.type('form.LoginForm input[name=session\\[password\\]]', password);
-  await page.click('form.LoginForm input[type=submit]');
+  try {
+    const page = await browser.newPage();
+    page.setDefaultNavigationTimeout(60 * 1000);
+    await page.goto(`https://twitter.com/${twitterID}`,
+        {waitUntil: ['load', 'networkidle0']});
+    console.log(`Logging in to ${twitterID}`);
+    await page.type('form.LoginForm input[name=session\\[username_or_email\\]]', twitterID);
+    await page.type('form.LoginForm input[name=session\\[password\\]]', password);
+    await page.click('form.LoginForm input[type=submit]');
 
-  let challenge_elem = await page.mainFrame().$('#challenge_response');
-  if (challenge_elem != null || challenge_elem != undefined) {
-    console.log(`Login challenge!`);
-    await page.type('#challenge_response', mail);
-    await page.click('#email_challenge_submit');
+    const challenge_elem = await page.mainFrame().$('#challenge_response');
+    if (challenge_elem != null || challenge_elem != undefined) {
+      console.log(`Login challenge!`);
+      await page.type('#challenge_response', mail);
+      await page.click('#email_challenge_submit');
+    }
+    console.log(`Open profile setting page`);
+
+    await page.goto(`https://twitter.com/settings/profile`),
+    await page.waitFor('div[data-testid=ProfileBirthdate_Edit_Button]');
+    await page.click('div[data-testid=ProfileBirthdate_Edit_Button]');
+    await page.waitFor('div[data-testid=confirmationSheetConfirm]');
+    await page.click('div[data-testid=confirmationSheetConfirm]');
+
+    console.log(`Setting ${twitterID}'s birthday to ` + getISOFormat(year, month, day));
+
+    await page.waitFor('select[data-testid=ProfileBirthdate_Year_Selector]');
+    await page.select('select[data-testid=ProfileBirthdate_Year_Selector]', year);
+    await page.select('select[data-testid=ProfileBirthdate_Month_Selector]', month);
+    await page.select('select[data-testid=ProfileBirthdate_Day_Selector]', day);
+
+    console.log(`Save birthday`);
+
+    await page.click('div[data-testid=Profile_Save_Button]');
+    await page.waitFor('div[data-testid=confirmationSheetConfirm]');
+    await page.click('div[data-testid=confirmationSheetConfirm]');
+    await page.waitFor(4000); //適当に待つ
+
+    console.log('done!');
+  } finally {
+    await browser.close();
   }
-  console.log(`Open profile setting page`);
-
-  await page.goto(`https://twitter.com/settings/profile`),
-  await page.waitFor('div[data-testid=ProfileBirthdate_Edit_Button]');
-  await page.click('div[data-testid=ProfileBirthdate_Edit_Button]');
-  await page.waitFor('div[data-testid=confirmationSheetConfirm]');
-  await page.click('div[data-testid=confirmationSheetConfirm]');
-
-  console.log(`Setting ${twitterID}'s birthday to ` + getISOFormat(year, month, day));
-
-  await page.waitFor('select[data-testid=ProfileBirthdate_Year_Selector]');
-  await page.select('select[data-testid=ProfileBirthdate_Year_Selector]', year);
-  await page.select('select[data-testid=ProfileBirthdate_Month_Selector]', month);
-  await page.select('select[data-testid=ProfileBirthdate_Day_Selector]', day);
-
-  console.log(`Save birthday`);
-
-  await page.click('div[data-testid=Profile_Save_Button]');
-  await page.waitFor('div[data-testid=confirmationSheetConfirm]');
-  await page.click('div[data-testid=confirmationSheetConfirm]');
-  await page.waitFor(4000); //適当に待つ
-
-  console.log('done!');
-
-  await browser.close();
 })();
